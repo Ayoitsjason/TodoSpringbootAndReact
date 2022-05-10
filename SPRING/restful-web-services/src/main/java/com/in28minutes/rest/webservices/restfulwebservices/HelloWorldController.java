37e50a95bde56c8547608ebec53dd276185e2149
Jason Le <jasonctle@gmail.com>
2022-05-10 15:53:46 -0700

    Added fix for CORS error in backend

--- SPRING/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ SPRING/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class HelloWorldController {
   
   //GET
@@ -17,14 +19,15 @@ public class HelloWorldController {
   }
 
   //hello-world-bean
-  @GetMapping(path="/hello-world-bean")
-  public HelloWorldBean helloWorldBean() {
-    return new HelloWorldBean("Hello World");
-  }
+  @GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
 
   //hello-world/path-variable/{name}
   @GetMapping(path="/hello-world/path-variable/{name}")
   public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+    // throw new RuntimeException("Something went wrong");
     return new HelloWorldBean(String.format("Hello World, %s", name));
   }
 }
